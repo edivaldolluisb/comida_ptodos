140efbee29160f166afdc720dbacbb9028abc4fd
Edivaldo <edivaldoluisb16@gmail.com>
2022-01-24 09:42:30 +0000

    finalizar e cancelar processos

--- estabelecimento/js/processo_finalizado.js
+++ estabelecimento/js/processo_finalizado.js
@@ -1,6 +1,8 @@
 
 /*Estabelecimento*/
 
+
+/*
 var cancelar_reserva = window.document.querySelector('.estabelecimento_cancelar_reserva')
 var finalizar_reserva = window.document.querySelector('.estabelecimento_finalizar_reserva')
 
@@ -14,4 +16,25 @@ function est_finalizar_reserva() {
 function est_cancelar_reserva() {
     history.go(-1)
 }
+*/
 
+
+/*fazer atualização*/
+
+var btn_f_atualizacao = window.document.querySelector('#f_atualizacao')
+var p_finalizado_succes = window.document.querySelector('#processo_finalizado_succes')
+
+
+btn_f_atualizacao.addEventListener('click', f_atualizacao)
+
+function f_atualizacao() {
+    p_finalizado_succes.style.display = 'grid'
+}
+
+/*cancelar ação*/
+
+var cancel = window.document.querySelector('#cancel')
+cancel.addEventListener('click', cancel_action)
+function cancel_action() {
+    history.go(-2)
+}

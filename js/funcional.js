50701b65d280dc2f91b7f941ca6d1e7dc0b71139
Edivaldo <edivaldoluisb16@gmail.com>
2022-01-25 16:18:55 +0000

    processo de validação feito
    implementado usando local storage
    
    updated pages:
    -inciar sessão;
    -funcional js

--- js/funcional.js
+++ js/funcional.js
@@ -11,7 +11,7 @@ registar_user.addEventListener("click", function () {
 
 
     let tipo_de_usuario = sel.value;
-    switch (tipo_de_usuario) {
+    /*switch (tipo_de_usuario) {
         case 'cliente_comum':
             console.log('tipo de usuario(cliente_comum)')
             window.location = 'https://edivaldolluisb.github.io/comida_ptodos/reservas_categoria.html'
@@ -25,8 +25,35 @@ registar_user.addEventListener("click", function () {
             window.location = 'https://edivaldolluisb.github.io/comida_ptodos/associacao/reservas_categoria.html'
             break;
 
+    }*/
+
+    var nome_comp = window.document.querySelector('input[type=text]').value
+    var email = window.document.querySelector('input[type=email]').value
+    var nif = window.document.querySelector('input[type=number]').value
+    var _password = window.document.querySelectorAll('input[type=password]')[0].value
+    var password_cof = window.document.querySelectorAll('input[type=password]')[1].value
+    console.log(nome_comp)
+    console.log(email)
+    console.log(nif)
+    console.log(_password)
+    console.log(password_cof)
+    console.log(sel.value)
+
+    user = {
+        nome: nome_comp,
+        email:email,
+        nif:nif,
+        password:password_cof,
+        tipo: sel.value
     }
+    localStorage.clear()
+    localStorage.user = JSON.stringify(user)
+    usuario = JSON.parse(localStorage.user)
+    console.log(usuario.nome)
+    
+
 
 
 });
+
 

a5cf9cdcd40acf2ac9596b8a4e27818bc773abbd
Edivaldo <edivaldoluisb16@gmail.com>
2022-01-26 01:53:37 +0000

    updated doar dinheiro

--- js/doar_dinheiro.js
+++ js/doar_dinheiro.js
@@ -1,5 +1,7 @@
 
 function pegar_id(clicked_id){
+
+    console.log(clicked_id)
     /*alert(clicked_id)*/
 
     /* Get the text field */
@@ -14,6 +16,21 @@ function pegar_id(clicked_id){
     console.log("Copied the text: " + copyText);
 
 
+}
+
+/*colcar dados do cartão para pagamento*/
+var sessao_pagamento = window.document.querySelector('#quantidade_pagamento')
+var add_dados_card = window.document.querySelector('#adicionar_dados_cartao')
+var fechar_sessao_doar = window.document.querySelector('#fechar_dados_card')
+
+fechar_sessao_doar.addEventListener('click', fechar_sessao)
+
+function abrir_sessao() {
+    sessao_pagamento.style.display = 'flex'
+    add_dados_card.style.display = 'flex'
+}
+function fechar_sessao() {
+    sessao_pagamento.style.display = 'none'
 }
 
 

78a633d144bb1c215883d5d781d79fc719be336e
Edivaldo <edivaldoluisb16@gmail.com>
2022-02-03 16:01:45 +0000

    alteração da parte das doações
    os clientes passam a doar produtos em vez de dinheiro
    
    - alteração da parte que o cliente pode fazer a doação, essa parte encontra-se na pag de visualização do produto
    + parte em que a associação possa ver as doações feitas para si

--- associacao/js/pedidos.js
+++ associacao/js/pedidos.js
@@ -1,3 +1,77 @@
+
+/*pedidos e doações*/
+
+var pedido_btn = window.document.querySelector('#pedidos_doacoes').querySelectorAll('button')[0]
+var doacao_btn = window.document.querySelector('#pedidos_doacoes').querySelectorAll('button')[1]
+
+var sessao_pedidos = window.document.querySelector('#listar_produtos')
+var sessao_doacao = window.document.querySelector('#listar_doacoes')
+
+pedido_btn.addEventListener('click', pedido)
+doacao_btn.addEventListener('click', doacao)
+
+function pedido() {
+    console.log('pedido')
+    sessao_doacao.style.display = 'none'
+    sessao_pedidos.style.display = 'block'
+}
+
+function doacao() {
+    console.log('doacao')
+    sessao_pedidos.style.display = 'none'
+    sessao_doacao.style.display = 'block'
+}
+
+
+
+/*notificação*/
+
+/* verificar se o browser suporta notificação*/
+if (!window.Notification) {
+    console.log('Browser does not support notifications.');
+} else {
+    console.log('Suporta notificação')
+}
+
+/*pedir permissão*/
+console.log(Notification.permission);
+
+if (Notification.permission === "granted") {
+    console.log("we have permission");
+} else if (Notification.permission !== "denied") {
+    alert('Conceda permissão ao site')
+    Notification.requestPermission().then(permission => {
+        console.log(permission);
+    });
+}
+
+/*mostrar notificação */
+function showNotification(paragrafo, mensagem) {
+    
+    var x, i;
+    /* x = document.querySelectorAll(".nome_estabelecimento"); */
+    x = paragrafo;
+
+    for (i = 0; i < x.length; i++) {
+        
+        const notification = new Notification("Comida p'ra todos", {
+            body: mensagem + x[i].innerText,
+            icon: "../imagens/favicon.png"
+        })
+        notification.onclick = (e) => {
+            window.location.href = "https://edivaldolluisb.github.io/comida_ptodos/associacao/pedidos.html";
+        };
+    } 
+
+}
+showNotification(paragrafo = document.querySelectorAll(".nome_estabelecimento"), mensagem = "O seu pedido foi aceite por " )
+
+showNotification(paragrafo = document.querySelectorAll(".doacao_recebida"), mensagem = "Você recebeu uma doação de" )
+
+
+
+
+
 /*function myFunction() {
     var x, i;
     x = document.querySelectorAll(".example");
@@ -34,45 +108,4 @@ function myFunction() {
 myFunction()*/
 
 
-
-/*notificação*/
-
-/* verificar se o browser suporta notificação*/
-if (!window.Notification) {
-    console.log('Browser does not support notifications.');
-} else {
-    console.log('Suporta notificação')
-}
-
-/*pedir permissão*/
-console.log(Notification.permission);
-
-if (Notification.permission === "granted") {
-    console.log("we have permission");
-} else if (Notification.permission !== "denied") {
-    alert('Conceda permissão ao site')
-    Notification.requestPermission().then(permission => {
-        console.log(permission);
-    });
-}
-
-/*mostrar notificação */
-function showNotification() {
-
-    var x, i;
-    x = document.querySelectorAll(".nome_associacao");
-
-    for (i = 0; i < x.length; i++) {
-        
-        const notification = new Notification("Comida p'ra todos", {
-            body: "O seu pedido foi aceite por " + x[i].innerText,
-            icon: "../imagens/favicon.png"
-        })
-        notification.onclick = (e) => {
-            window.location.href = "https://edivaldolluisb.github.io/comida_ptodos/associacao/pedidos.html";
-        };
-    }
-
-}
-showNotification()
 
